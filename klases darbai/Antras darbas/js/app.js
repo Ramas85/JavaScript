a660fe2ae0a1d9996c9ca1992eab1366b9aadc44
Ramunas <ramasit85@gmail.com>
2020-10-23 11:34:56 +0300

    JS some changes

--- klases darbai/Antras darbas/js/app.js
+++ klases darbai/Antras darbas/js/app.js
@@ -1,8 +1,9 @@
 // self invoking funcion
 (function() {
 
-    const article = document.querySelector('article');
-    const articles = document.querySelectorAll('article');
+
+    // const articles = document.querySelectorAll('article');
+    // const links = document.querySelectorAll('a');
     // console.log(article);
 
     // article.addEventListener('click', changeBg);
@@ -11,9 +12,33 @@
     //     article.style.background = 'tomato';
     // }
 
-    for (let i = 0; i < articles.length; i++) {
-        articles[i].addEventListener('click', function() {
-            articles[i].style.background = 'tomato';
-        });
+    // for (let i = 0; i < links.length; i++) {
+    //     // 
+    //     {
+    //         links[i].href = 'http://www.kitm.lt';
+    //     }
+    // }
+
+    const article = document.querySelector('article h3');
+
+
+
+
+
+    // function changeTitle() {
+    //     // article.textContent = userInput.value;  //neleidzia html
+    //     article.innerHTML = userInput.value; //leidzia html
+    // }
+    const list = document.querySelector('ul');
+    const btn = document.querySelector('button');
+    const userInput = document.querySelector('input')
+
+    btn.addEventListener('click', addTask);
+
+    function addTask() {
+        let task = document.createElement('ul'); //sukuriau list;
+        task.textContent = userInput.value; // i li  idejau turini
+        list.appendChild(task); // li idejau i ul vidu
     }
+
 })();
